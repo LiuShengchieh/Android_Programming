d499b02a233a9d5060b003523d172b5a85241b10
Shengjie <liushengchieh@gmail.com>
2018-10-29 23:10:34 +0800

    第20章

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * 项目名：BeatBox
+ * 包名：  com.bignerdranch.android.beatbox
+ * 文件名：Sound
+ * Created by liushengjie on 2018/10/29.
+ * 描述：
+ */
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+
+}
---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.java
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+/**
+ * 项目名：BeatBox
+ * 包名：  com.bignerdranch.android.beatbox
+ * 文件名：SoundViewModel
+ * Created by liushengjie on 2018/10/29.
+ * 描述：
+ */
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
