de27ec305a2344f57fdc4f9747c7891257e07d92
shengjie.liu <shengjie.liu@542deMacBook-Air.local>
2018-11-21 19:32:43 +0800

    第31章

---
+++ DragAndDraw/app/src/main/java/com/example/shengjieliu/draganddraw/Box.java
@@ -0,0 +1,25 @@
+package com.example.shengjieliu.draganddraw;
+
+import android.graphics.PointF;
+
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+}
