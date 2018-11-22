6f113fd8a6aba8aa42bd60ca528c4a5dad7467eb
shengjie.liu <shengjie.liu@542deMacBook-Air.local>
2018-11-22 18:06:02 +0800

    第32章

---
+++ Sunset/app/src/main/java/com/example/shengjieliu/sunset/SingleFragmentActivity.java
@@ -0,0 +1,43 @@
+package com.example.shengjieliu.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 项目名：CriminalIntent
+ * 包名：  com.bignerdranch.android.criminalintent
+ * 文件名：SingleFragmentActivity
+ * Created by liushengjie on 2018/10/8.
+ * 描述：
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+}
